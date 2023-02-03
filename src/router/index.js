dbdd48fad62b7f2520cd7efca2c14d79f52b9d79
heshuchang <heshuchang.wh@haier.com>
2023-02-03 17:12:28 +0800

    增加分类参数模块

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Users from '../components/user/Users.vue'
 import Rights from '../components/power/rights.vue'
 import Roles from "../components/power/Roles.vue"
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -55,10 +56,13 @@ const routes = [
         path: "/roles",
         component: Roles,
       },
-      ,
       {
         path: "/categories",
         component: Cate,
+      },
+      {
+        path: "/params",
+        component: Params,
       },
     ],
   },
