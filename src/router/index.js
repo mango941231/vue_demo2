a522b2eb4e9597be5705d30680b7a7649d2b2700
heshuchang <heshuchang.wh@haier.com>
2023-02-09 13:21:03 +0800

    完成商品功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,8 @@ import Rights from '../components/power/rights.vue'
 import Roles from "../components/power/Roles.vue"
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import List from "../components/goods/List.vue";
+import Add from '../components/goods/Add.vue'
 
 Vue.use(VueRouter)
 
@@ -63,6 +65,14 @@ const routes = [
       {
         path: "/params",
         component: Params,
+      },
+      {
+        path: "/goods",
+        component: List,
+      },
+      {
+        path: "/goods/add",
+        component: Add,
       },
     ],
   },
